3f1d9e2825d6cb6660f36c10b458768423841357
Grattours <grattours@gmail.com>
2019-11-22 15:19:58 +0100

    D59 - réorganisation vues - all in one )- Cha

---
+++ CoreDataProjectIntro/View/FilteredListView.swift
@@ -0,0 +1,59 @@
+//
+//  FilteredListView.swift
+//  CoreDataProjectIntro
+//
+//  Created by Luc Derosne on 22/11/2019.
+//  Copyright © 2019 Luc Derosne. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilteredListView: View {
+    @Environment(\.managedObjectContext) var moc
+    @State var lastNameFilter = "A"
+
+    var body: some View {
+        VStack {
+            //FilteredList(filter: lastNameFilter)
+//            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer)
+            FilteredList(predicate: .beginsWith,
+            filterKey: "lastName",
+            filterValue: lastNameFilter,
+            sortKey: "lastName",
+            sortAsc: true) { (singer: Singer)
+                in
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            }
+            Button("Add Examples") {
+                let taylor = Singer(context: self.moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
+
+                let ed = Singer(context: self.moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
+
+                let adele = Singer(context: self.moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
+
+                try? self.moc.save()
+            }
+
+            Button("Show A") {
+                self.lastNameFilter = "A"
+            }
+
+            Button("Show S") {
+                self.lastNameFilter = "S"
+            }
+        }
+
+    }
+}
+
+struct FilteredListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteredListView()
+    }
+}
